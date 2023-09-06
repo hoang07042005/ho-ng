41ee3ad00c9cacdc6af9a5fac87517c6dcbe7962
hoang07042005 <142959468+hoang07042005@users.noreply.github.com>
2023-09-06 11:20:13 +0700

    Add files via upload

--- 7.7.c
+++ 7.7.c
@@ -3,17 +3,16 @@ void main()
 {
 	int x, y;
 	x = y = 0;
-	clrscr();
 	printf("Enter Choice (1 - 3): ");
 	scanf("%d", &x);
 	if(x == 1)
 	{
-		printf("\nEnter valua for y (1 - 5): ");
-		scanf("%d", &y);
+		printf ("\nEnter valua for y (1 - 5): ");
+		scanf ("%d", &y);
 		if (y <= 5)
-		printf("\nThe value for y is: %d", y);
+		printf ("\nThe value for y is: %d", y);
 		else
-		printf("\nThe value of y exceedds 5");
+		printf ("\nThe value of y exceeds 5");
 	}
 	else
 	    printf ("\nChoice entered was not 1");
