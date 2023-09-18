87d9c13623b0f3e5c33ef18e82564d0fc7ee34d2
hoang07042005 <142959468+hoang07042005@users.noreply.github.com>
2023-09-18 10:50:39 +0700

    Add files via upload

---
+++ ham menu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void drawMenu(){
+	printf("Tran chau duong den\t\t25k\n");
+	printf("tra chanh lo hoi\t\t\t25k\n");
+	printf("tra dao bon mua\t\t25k\n");
+	printf("hong tra chanh\t\t25k\n");
+	printf("tra dao big size\t\t25k\n");
+	printf("tra o long bon mua\t\t25k\n");
+	printf("tra sua tran chau duong den\t\t30k\n");
+	printf("tra ba vuong\t\t25k\n");
+	printf("tra sua nuong\t\t25k\n");
+}
+void main(){
+	printf("menu\n");
+	drawMenu();
+}
+
+
