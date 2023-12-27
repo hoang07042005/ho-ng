8a48867b389594c134393a57b1369c0eb666160d
hoang07042005 <142959468+hoang07042005@users.noreply.github.com>
2023-12-27 11:54:00 +0700

    Update bảng bán hàng.sql

--- bảng bán hàng.sql
+++ bảng bán hàng.sql
@@ -1,34 +1,62 @@
-﻿CREATE TABLE DonDatHang (
-    MaSDH INT PRIMARY KEY,
-    NguoiDH VARCHAR(255),
-    DiaC VARCHAR(255),
-    DienT VARCHAR(15),
-    NgayDH DATE
-)
-SELECT * FROM DonDatHang
+CREATE TABLE Customer (
+	CustomerID INT PRIMARY KEY,
+	Name NVARCHAR (40),
+	Address NVARCHAR (80),
+	Tel VARCHAR (10),
+	Status VARCHAR (30)
+);
 
-INSERT INTO DonDatHang (MaSDH, NguoiDH, DiaC, DienT, NgayDH)
-VALUES (123, 'Nguyễn Văn An', '111 Nguyễn Trãi, Thanh Xuân, Hà Nội', '987654321', '2009-09-18');
+INSERT INTO Customer VALUES (1, 'Nguyên Thế Sơn', 'CT1-1 KĐT Mễ Trì Hạ', 0888317598, NULL);
+INSERT INTO Customer VALUES (2, 'Đỗ Việt Hoàng', 'Phú Diễn, Kiều Mai, Hà Nội', 0352335720, NULL);
+INSERT INTO Customer VALUES (3, 'Nguyễn Văn An', '111 Nguyễn Trãi, Thanh Xuân, Hà Nội', 987654321, NULL);
 
-SELECT * FROM DonDatHang
+DROP TABLE Customer;
+SELECT * FROM Customer;
 
-CREATE TABLE DangSachCacMatHang (
-    MaSDH INT,
-    ItemID INT PRIMARY KEY,
-    TenHang VARCHAR(255),
-    MoTaHang VARCHAR(255),
-    DonVi VARCHAR(20),
-    Gia DECIMAL(10, 2),
-    SoLuong INT,
-    ThanhTien DECIMAL(10, 2),
-    FOREIGN KEY (MaSDH) REFERENCES DonDatHang(MaSDH)
-)
+CREATE TABLE Product (
+	ProductID INT PRIMARY KEY,
+	Name NVARCHAR (40),
+	Description NVARCHAR (40),
+	Unit NVARCHAR (10),
+	Price INT,
+	Quantity INT,
+	Status NVARCHAR (30)
+);
 
-INSERT INTO DangSachCacMatHang (MaSDH, ItemID, TenHang, MoTaHang, DonVi, Gia, SoLuong, ThanhTien)
- VALUES	(123, 1, 'Máy Tính T450', 'Máy nhập mới', 'Chiếc', 1000, 1, 1000),
-	(123, 2, 'Điện Thoại Nokia5670', 'Điện thoại đang hot', 'Chiếc', 200, 2, 400),
-	(123, 3, 'Máy In Samsung 450', 'Máy in đang ế', 'Chiếc', 100, 1, 100);
-
-SELECT * FROM DangSachCacMatHang
+INSERT INTO Product VALUES (1, 'Máy Tính T450', 'Máy nhập mới', 'chiếc', 1000, 20, 'In Stock');
+INSERT INTO Product VALUES (2, 'Điện Thoại Nokia 5670', 'Điện thoại đang hot', 'chiếc', 200, 10, 'In Stock');
+INSERT INTO Product VALUES (3, 'Máy In Samsung 450', 'Máy in đang ế', 'chiếc', 100, 15, 'In Stock');
 
 
+CREATE TABLE Orders (
+	OrderID INT PRIMARY KEY,
+	CustomerID INT,
+	OrderDate DATE,
+	Status NVARCHAR (30)
+	FOREIGN KEY (CustomerID) REFERENCES Customer (CustomerID)
+);
+
+INSERT INTO Orders VALUES (1, 2, '11-18-13', 'In Process');
+INSERT INTO Orders VALUES (123, 3, '11-18-19', 'In Process');
+INSERT INTO Orders VALUES (13, 1, '12-12-23', 'In Process');
+
+
+SELECT * FROM Customer;
+SELECT * FROM Product;
+SELECT * FROM Orders;
+
+SELECT DISTINCT Name FROM Customer
+
+SELECT DISTINCT Name FROM Product
+
+
+
+
+
+SELECT * FROM Customer ORDER BY Name
+
+SELECT * FROM Product ORDER BY Price DESC
+
+SELECT COUNT(DISTINCT CustomerID) AS TotalCustomers FROM Customer
+
+SELECT COUNT(DISTINCT ProductID) AS TotalItems FROM Product
